68fbe5e28dfd09ebca11331f7cf3d34898a24955
Reland Boyle <reland.boyle@gmail.com>
2022-01-26 02:24:29 -0500

    http

--- routes/passport.js
+++ routes/passport.js
@@ -21,7 +21,7 @@ passport.deserializeUser(function(user, done) {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback"
+    callbackURL: "http://interview-skills-grid.herokuapp.com/auth/google/callback"
     },
     function(accessToken, refreshToken, profile, cb) {
         User.findOrCreate({
